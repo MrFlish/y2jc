bb22171bcf87e44fa405347dd53f9157b75b0bc0
Bertrand COEZ <coez.bertrand@neuf.fr>
2022-05-28 00:12:51 +0200

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import path from "path";
 import { Compiler } from "./classes/compiler";
 import Source from "./classes/IODirectory/source";
 import Target from "./classes/IODirectory/target";
+import { ModuleConfiguration } from "./classes/configuration";
+import { configurationSchema } from "./validation/configurationSchemas";
 
-const srce = path.join(__dirname, "../configuration/src")
-const trgt = path.join(__dirname, "../configuration/json")
-
-const main = async function(){
-
-    const source = new Source(srce);
-    const target = new Target(trgt)
-    const compiler = new Compiler(source, target);
-    await compiler.init();
-    await compiler.watch();
-
-    return Promise.resolve();
+const main = async function(){   
+    const configuration = new ModuleConfiguration("json.compiler.yaml", configurationSchema);
+    await configuration.find(__dirname, 5);
+    await configuration.load();
+    const options = configuration.options;
+    const files = configuration.files;
+    for(const pair of files){
+        const compiler = new Compiler(new Source(pair.source), new Target(pair.target), options);
+        await compiler.init();
+        await compiler.watch();
+    }
+    return;
 }
-
-
 main();
